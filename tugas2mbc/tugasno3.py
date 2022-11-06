f8606603a7f8b87df89f633051312317d10999c7
fajrul falah al faruqi <alfaruqi2001@gmail.com>
2022-11-06 13:28:34 +0700

    menentukan warna baju

---
+++ tugas2mbc/tugasno3.py
@@ -0,0 +1,18 @@
+print ("Program Menentukan Warna Baju")
+print("==============================")
+Hari=['Senin','Selasa','Rabu','Kamis','Jumat','Sabtu','Minggu',]
+Hari = str(input("Masukkan Hari:"))
+if(Hari== "Senin"):
+        print("Pada Hari Senin memakai baju merah.")
+elif(Hari== "Selasa"):
+        print("Pada Hari Senin memakai baju putih.")
+elif(Hari== "Rabu"):
+        print("Pada Hari Senin memakai baju putih.")
+elif(Hari== "Kamis"):
+        print("Pada Hari Senin memakai baju bebas.")
+elif(Hari== "Jumat"):
+        print("Pada Hari Senin memakai baju batik.")
+elif(Hari== "Sabtu"):
+        print("Pada Hari Senin memakai baju bebas.")
+else:
+        print("salah")
